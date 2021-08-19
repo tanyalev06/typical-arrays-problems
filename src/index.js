71c789ad1b888e8dab5d19d0bd9999452acddbfa
Tatyana Levchenko <grib.tanya06@mail.ru>
2021-08-19 23:58:46 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }
+    else {
+        var min = Math.min.apply(null, array);
+        return (min);
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }
+    else {
+        var max = Math.max.apply(null, array);
+        return (max);
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0){
+        return 0;
+    }
+    else {
+    let sum = 0;
+    for (i=0; i< array.length; i++){
+      
+       sum += array[i];
+    }
+    return avg=sum/array.length;
+}
 }
